06adb35ae63752fb9cc31e78ed23906a58d99af9
andre <andregabschuh@gmail.com>
2025-06-05 14:04:29 -0300

    Melhorando o consumo da api e design

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
-angular.module('blog', []);
+// Cria um módulo (blogApp) (sem dependencias)
+angular.module('blogApp', [])
 
-// Controller da lista de postagens
-angular.module('blog').controller('Rest', function ($scope, $http){
-  $http.get('https://api-fake-blog.onrender.com/postagens')
-    .success(function(data) {
-      $scope.publicacoes = data;
+// controller (BlogCtrl) listagem de postagens
+.controller('BlogCtrl', function($scope, $http) {
+  $scope.carregando = true;
+  $scope.erro = false;
+
+  //requisição GET para postagens
+  $http.get('https://api-fake-blog.onrender.com/postagens/')
+    .then(function(response) {
+      //armazena os dados da resposta na variável 'posts'
+      $scope.posts = response.data;
+      //carregamento terminou
+      $scope.carregando = false;
+    }, function() {
+      //Caso de erro requisição:
+      $scope.erro = true;
+      //carregamento terminou
+      $scope.carregando = false;
     });
-});
+})
 
-// Controller da postagem individual
-angular.module('blog').controller('PostagemCtrl', function ($scope, $http, $window){
-  // Função para extrair o parâmetro da URL
+// controller (PostCtrl) postagem específica
+.controller('PostCtrl', function($scope, $http, $location) {
+  //mesmo sistema de cima
+  $scope.carregando = true;
+  $scope.erro = false;
+
+  //Para obter o id da API
   function getIdFromUrl() {
-    const params = new URLSearchParams($window.location.search);
+    var params = new URLSearchParams(window.location.search);
+    //valor do parâmetro 'id'
     return params.get('id');
   }
 
-  const id = getIdFromUrl();
-  if (id) {
+  //id da API
+  var id = getIdFromUrl();
+
+  //id existe e é um número?
+  if (id !== null && !isNaN(Number(id))) {
+    // GET para obter o id de x postagem
     $http.get('https://api-fake-blog.onrender.com/postagem/' + id)
-      .success(function(data) {
-        $scope.postagem = data;
-      })
-      .error(function() {
-        $scope.postagem = { title: 'Erro', description: 'Não foi possível carregar a postagem.' };
+      .then(function(response) {
+        //mesmo sistema la de cima
+        $scope.post = response.data;
+        $scope.carregando = false;
+      }, function() {
+        $scope.erro = true;
+        $scope.carregando = false;
       });
+  } else {
+    // id inválido = erro
+    $scope.erro = true;
+    $scope.carregando = false;
   }
 });
